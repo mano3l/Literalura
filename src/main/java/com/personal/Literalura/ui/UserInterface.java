0446828007aa171e2031aaf045045d01a8f52853
Manoel <fonsecadesouzamanoel@gmail.com>
2024-07-12 12:14:32 -0300

    ui setup

---
+++ src/main/java/com/personal/Literalura/ui/UserInterface.java
@@ -0,0 +1,14 @@
+package com.personal.Literalura.ui;
+
+public class UserInterface {
+    public void start() {
+        System.out.println("--------------------");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1- Buscar livro pelo título");
+        System.out.println("2- Listar livros registrados");
+        System.out.println("3- Listar autores registrados");
+        System.out.println("4- Listar autores vivos em um determinado ano");
+        System.out.println("5- Listar livros em um determinado idioma");
+        System.out.println("0- Sair");
+    }
+}
